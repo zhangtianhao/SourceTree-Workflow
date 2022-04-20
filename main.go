8337633ddefcd428ca4969295c45285d18687c31
张彬 <2321320300@qq.com>
2022-04-20 12:57:56 +0800

    搜索时支持更灵活的匹配

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/zhangtianhao/SourceTree-Workflow/homedir"
 	"howett.net/plist"
 	"io/ioutil"
+	"path"
 	"reflect"
 	"strings"
 	"unicode"
@@ -21,16 +23,28 @@ func splitString(r rune) bool {
 	return r == '_' || r == '-' || unicode.IsSpace(r)
 }
 
+// k8s-docker-desktop-for-mac
+// k8s docker desktop for mac
+// k8s-docker k8s-docker-desktop k8s-docker-desktop-for k8s-docker-desktop-for-mac
+// docker-desktop docker-desktop-for docker-desktop-for-mac
+// desktop-for desktop-for-mac
+// for-mac
 func splitMatch(title string) string {
 	fragments := strings.FieldsFunc(title, splitString)
-	if len(fragments) > 1 {
-		return title + " " + strings.Join(fragments, " ")
+	length := len(fragments)
+	for i := 0; i < length; i++ {
+		temp := fragments[i]
+		for j := i + 1; j < length; j++ {
+			temp = temp + "-" + fragments[j]
+			fragments = append(fragments, temp)
+		}
 	}
-	return title
+	return strings.Join(fragments, " ")
 }
 
 func getList() *AlfredList {
-	bs, _ := ioutil.ReadFile("/Users/user/Library/Application Support/SourceTree/browser.plist")
+	dir, _ := homedir.Dir()
+	bs, _ := ioutil.ReadFile(path.Join(dir, "/Library/Application Support/SourceTree/browser.plist"))
 	var stp SourceTreePlist
 	_, _ = plist.Unmarshal(bs, &stp)
 	alfredSlice := make([]AlfredItem, 0, len(stp.Objects))
